1f7bbfa7964fa5119825304756b45cd5f38c8c8e
dimitar-trposki <dimitar.curry30@gmail.com>
2025-11-11 21:43:16 +0100

    LE2A almost done

---
+++ LabA/src/main/java/mk/ukim/finki/wp/lab/web/controller/BookReservationController.java
@@ -0,0 +1,9 @@
+package mk.ukim.finki.wp.lab.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bookReservation")
+public class BookReservationController {
+}
